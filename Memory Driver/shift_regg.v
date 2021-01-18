5822daf3271e0932b6878d6e86ef43e0402187b7
EmilijaV <76113883+emilijav-97@users.noreply.github.com>
2021-01-18 11:15:48 +0200

    Update shift_regg.v

--- Memory Driver/shift_regg.v
+++ Memory Driver/shift_regg.v
@@ -1,13 +1,14 @@
-//SIPO reg
-module shift_regg (	input clk, si, shift,
-							output po
-						);
+//SIPO reg has been used to count 4 counter outputs.
+module shift_regg (	input clk, si, validin
+		    	input  [7:0] shift,
+			output po
+		    );
 						
 	reg [31:0] data;
 	
-	always @(posedge clk)
+	always @(posedge clk or validin)
 		begin
-			if (shift == 1'b0)
+			if (shift == 8'b0 && validin == 1'b0)
 				begin
 					data = {data [30:0], si};
 				end
